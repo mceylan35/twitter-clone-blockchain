c11129ef2816661e57f303fe9707e04a32828914
mceylan35 <mehmetceylan2696@gmail.com>
2022-05-22 02:43:22 +0300

    twittercontext eklendi

--- pages/context/TwitterContext.js
+++ pages/context/TwitterContext.js
@@ -1,11 +1,50 @@
 import { createContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import {client} from '.'
+import { client } from '../lib/client'
 export const TwitterContext = createContext()
-const TwitterContext = () => {
-  return (
-    <div>TwitterContext</div>
-  )
+
+export const TwitterProvider=({children})=>{
+
+  const [appStatus, setAppStatus] = useState('')
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [currentUser, setCurrentUser] = useState({})
+  const [tweets, setTweets] = useState([])
+  const router=useRouter();
+  useEffect(()=>{
+    checkIfWalletIsConnected()
+  },[]);
+
+  useEffect(()=>{
+      if(!currentAccount && appStatus=='connected')return;
+      getCurrentUserDetails(currentAccount);
+      fetchTweets()
+
+    },[current, appStatus]);
+    
+    const checkIfWalletIsConnected=async()=>{
+        if(!window.ethereum)
+        return setAppStatus('noMetamask');
+        try {
+          const addressArray=await window.ethereum.request({
+            method:'eth_account'
+
+          })
+          if (addressArray.length>0) {
+            setAppStatus('connected');
+            setCurrentAccount(addressArray[0])
+
+            createUserAccount(addressArray[0]);
+          }
+          else{
+            router.push('/');
+            setAppStatus('notConnected');
+          }
+        } catch (error) {
+          router.push('/');
+          setAppStatus('error')
+        }
+    }
+
 }
 
 export default TwitterContext
