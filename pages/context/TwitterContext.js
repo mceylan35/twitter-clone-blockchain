ed39589f8a3e0c52e39c69532490cae5643ad809
mceylan35 <mehmetceylan2696@gmail.com>
2022-05-22 22:48:14 +0300

    twittercontext yeni eklemeler yapıldı

--- pages/context/TwitterContext.js
+++ pages/context/TwitterContext.js
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { client } from '../lib/client'
+import { create } from 'domain'
 export const TwitterContext = createContext()
 
 export const TwitterProvider=({children})=>{
@@ -45,6 +46,145 @@ export const TwitterProvider=({children})=>{
         }
     }
 
-}
+    const connectWallet=async()=>{
+      if (!window.ethereum) {
+        return setAppStatus('noMetamask');
+      }
+      try {
+        setAppStatus('loading');
+        const addressArray=await window.ethereum.request({
+          method:'eth_requestAccounts',
+        });
+        if (addressArray.length>0) {
+          setCurrentAccount(addressArray[0])
+          createUserAccount(addressArray[0]);
+
+        }
+        else{
+          router.push('/')
+          setAppStatus('notConnected');
+        }
+      } catch (error) {
+        setAppStatus('error')
+      }
+    }
+    const createUserAccount = async (userAddress = currentAccount) => {
+      if (!window.ethereum) return setAppStatus('noMetaMask')
+      try {
+        const userDoc = {
+          _type: 'users',
+          _id: userAddress,
+          name: 'Unnamed',
+          isProfileImageNft: false,
+          profileImage:
+            'https://about.twitter.com/content/dam/about-twitter/en/brand-toolkit/brand-download-img-1.jpg.twimg.1920.jpg',
+          walletAddress: userAddress,
+        }
+  
+        await client.createIfNotExists(userDoc)
+  
+        setAppStatus('connected')
+      } catch (error) {
+        router.push('/')
+        setAppStatus('error')
+      }
+    }
+    const getNftProfileImage = async (imageUri, isNft) => {
+      if (isNft) {
+        return `https://gateway.pinata.cloud/ipfs/${imageUri}`
+      } else if (!isNft) {
+        return imageUri
+      }
+    }
+    const fetchTweets = async () => {
+      const query = `
+        *[_type == "tweets"]{
+          "author": author->{name, walletAddress, profileImage, isProfileImageNft},
+          tweet,
+          timestamp
+        }|order(timestamp desc)
+      `
+  
+      // setTweets(await client.fetch(query))
+  
+      const sanityResponse = await client.fetch(query)
+  
+      setTweets([])
+  
+      /**
+       * Async await not available with for..of loops.
+       */
+      sanityResponse.forEach(async item => {
+        const profileImageUrl = await getNftProfileImage(
+          item.author.profileImage,
+          item.author.isProfileImageNft,
+        )
+  
+        if (item.author.isProfileImageNft) {
+          const newItem = {
+            tweet: item.tweet,
+            timestamp: item.timestamp,
+            author: {
+              name: item.author.name,
+              walletAddress: item.author.walletAddress,
+              profileImage: profileImageUrl,
+              isProfileImageNft: item.author.isProfileImageNft,
+            },
+          }
+  
+          setTweets(prevState => [...prevState, newItem])
+        } else {
+          setTweets(prevState => [...prevState, item])
+        }
+      })
+    }
+    const getCurrentUserDetails = async (userAccount = currentAccount) => {
+      if (appStatus !== 'connected') return
+  
+      const query = `
+        *[_type == "users" && _id == "${userAccount}"]{
+          "tweets": tweets[]->{timestamp, tweet}|order(timestamp desc),
+          name,
+          profileImage,
+          isProfileImageNft,
+          coverImage,
+          walletAddress
+        }
+      `
+      const response = await client.fetch(query)
+  
+      const profileImageUri = await getNftProfileImage(
+        response[0].profileImage,
+        response[0].isProfileImageNft,
+      )
+  
+      setCurrentUser({
+        tweets: response[0].tweets,
+        name: response[0].name,
+        profileImage: profileImageUri,
+        walletAddress: response[0].walletAddress,
+        coverImage: response[0].coverImage,
+        isProfileImageNft: response[0].isProfileImageNft,
+      })
+    }
+  
+    return (
+      <TwitterContext.Provider
+        value={{
+          appStatus,
+          currentAccount,
+          connectWallet,
+          tweets,
+          fetchTweets,
+          setAppStatus,
+          getNftProfileImage,
+          currentUser,
+          getCurrentUserDetails,
+        }}
+      >
+        {children}
+      </TwitterContext.Provider>
+    )
+  }
 
 export default TwitterContext
